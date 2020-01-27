e7366d27490c37f610efb1f8d0956589ec92229e
Patrick Waks <wazpys@gmail.com>
2020-01-27 16:26:11 -0600

    Added test case with console.log in  hook

---
+++ tests/testcase.test.js
@@ -0,0 +1,8 @@
+describe(`Error should be parsable in afterTest`, function () {
+    it(`should error out`, function () {
+        browser
+            .url(`/`)
+            
+        $(`.DoesNotExist`).waitForExist(500)
+    })
+})
